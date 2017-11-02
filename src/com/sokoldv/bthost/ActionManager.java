6676ea7c97454255bcc70bb7deec4e0d7098880f
SDemonUA <sokoldv@ukr.net>
2017-11-02 18:38:28 +0200

    Add working process to communicate with phone

--- src/com/sokoldv/bthost/ActionManager.java
+++ src/com/sokoldv/bthost/ActionManager.java
@@ -1,25 +1,74 @@
 package com.sokoldv.bthost;
 
-import sun.awt.image.ToolkitImage;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.io.IOException;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-import javax.tools.Tool;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.nio.file.Paths;
 
 public class ActionManager {
-    public static void main(String[] args) {
+    private static BluetoothActionService bluetoothActionService;
 
+    static JButton[] buttons = new JButton[3];
+    static String[] icons = new String[]{ "flash", "vibrate", "photo" };
+    static String[] actions = new String[]{ "flash", "vibrate", "photo" };
+    static String[] titles = new String[]{ "Ready", "Steady", "Go" };
+    static int[] borderColors = new int[]{ 0xc9def3, 0xc9ead1, 0xfbd2d0 };
 
+    public static void main(String[] args) throws IOException {
+        bluetoothActionService = new BluetoothActionService();
         JFrame window = initWindow();
 
+
+        new Thread(() -> {
+            final Object lock = new Object();
+
+            while (true){
+                bluetoothActionService.getAction(newAction -> {
+                    onActionChange(newAction);
+
+                    synchronized (lock){
+                        lock.notify();
+                    }
+                });
+
+                synchronized (lock){
+                    try { lock.wait(); }
+                    catch (InterruptedException e) { e.printStackTrace(); }
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    static void onActionChange(String action){
+        for (int i = 0; i < actions.length; i++) {
+            if (actions[i].equalsIgnoreCase(action)){
+                buttons[i].grabFocus();
+            }
+        }
     }
 
     static JFrame initWindow(){
@@ -44,22 +93,20 @@ public class ActionManager {
         buttonPanel.setBorder(new EmptyBorder(padding, padding, padding, padding));
         buttonPanel.setBackground(Color.WHITE);
 
-        // Buttons
-        String[] icons = new String[]{ "flash", "vibrate", "photo" };
-        String[] titles = new String[]{ "Ready", "Steady", "Go" };
-        int[] borderColors = new int[]{ 0xc9def3, 0xc9ead1, 0xfbd2d0 };
-
         for (int i = 0; i < 3; i++) {
-            JButton btn = new JButton(titles[i], new ImageIcon(String.format("./assets/%s.png", icons[i])));
+            ImageIcon icon = new ImageIcon(Toolkit.getDefaultToolkit().getImage(ActionManager.class.getResource(String.format("/%s.png", icons[i]))));
+            JButton btn = new JButton(titles[i], icon);
+            buttons[i] = btn;
+
             btn.setVerticalTextPosition(SwingConstants.BOTTOM);
             btn.setHorizontalTextPosition(SwingConstants.CENTER);
             btn.setFont(new Font("sans-serif", Font.PLAIN, 24));
             btn.setIconTextGap(40);
-            btn.setDefaultCapable(false);
+            btn.setDefaultCapable(true);
 
             btn.setBorderPainted(false);
             btn.setFocusPainted(false);
-            btn.setContentAreaFilled(false);
+            btn.setContentAreaFilled(true);
             btn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
             CompoundBorder border = BorderFactory.createCompoundBorder(
@@ -67,35 +114,65 @@ public class ActionManager {
                     BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
             btn.setBorder(border);
-            btn.setBackground(new Color(0xFAFAFA));
+            btn.setBackground(Color.WHITE);
 
-            btn.addMouseListener(new MouseAdapter() {
+            btn.addActionListener(new ActionListener() {
                 @Override
-                public void mousePressed(MouseEvent e) {
-                    super.mousePressed(e);
-                    System.out.println("pressed");
-                    ((JButton) e.getComponent()).setContentAreaFilled(true);
+                public void actionPerformed(ActionEvent e) {
+                    JButton source = (JButton) e.getSource();
+                    System.out.println(source.getText());
+                    String item = source.getText();
+                    for (int i1 = 0; i1 < titles.length; i1++) {
+                        if (titles[i1].equals(item)){
+                            bluetoothActionService.setAction(actions[i1], ActionManager::onActionChange);
+                            break;
+                        }
+                    }
+                }
+            });
+//            btn.addMouseListener(new MouseAdapter() {
+//                @Override
+//                public void mousePressed(MouseEvent e) {
+//                    super.mousePressed(e);
+//                    System.out.println("pressed");
+//                }
+//
+//                @Override
+//                public void mouseReleased(MouseEvent e) {
+//                    super.mouseReleased(e);
+//                    System.out.println("released");
+//                }
+//
+//                @Override
+//                public void mouseEntered(MouseEvent e) {
+//                    super.mouseEntered(e);
+//                    System.out.println("entered");
+//                    ((JButton) e.getComponent()).setBorderPainted(true);
+//                }
+//
+//                @Override
+//                public void mouseExited(MouseEvent e) {
+//                    super.mouseExited(e);
+//                    System.out.println("exited");
+//                    ((JButton) e.getComponent()).setBorderPainted(false);
+//                }
+//            });
+
+            btn.addFocusListener(new FocusAdapter() {
+                @Override
+                public void focusGained(FocusEvent e) {
+                    super.focusGained(e);
+                    JButton button = (JButton) e.getComponent();
+                    button.setBackground(new Color(0xfafafa));
+                    button.setBorderPainted(true);
                 }
 
                 @Override
-                public void mouseReleased(MouseEvent e) {
-                    super.mouseReleased(e);
-                    System.out.println("released");
-                    ((JButton) e.getComponent()).setContentAreaFilled(false);
-                }
-
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    super.mouseEntered(e);
-                    System.out.println("entered");
-                    ((JButton) e.getComponent()).setBorderPainted(true);
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    super.mouseExited(e);
-                    System.out.println("exited");
-                    ((JButton) e.getComponent()).setBorderPainted(false);
+                public void focusLost(FocusEvent e) {
+                    super.focusLost(e);
+                    JButton button = (JButton) e.getComponent();
+                    button.setBackground(Color.WHITE);
+                    button.setBorderPainted(false);
                 }
             });
 
@@ -110,44 +187,3 @@ public class ActionManager {
     }
 }
 
-class MyButton extends JButton{
-    public MyButton() {
-        super();
-    }
-
-    public MyButton(Icon icon) {
-        super(icon);
-    }
-
-    public MyButton(String text) {
-        super(text);
-    }
-
-    public MyButton(Action a) {
-        super(a);
-    }
-
-    public MyButton(String text, Icon icon) {
-        super(text, icon);
-    }
-
-    @Override
-    protected void paintBorder(Graphics g) {
-        super.paintBorder(g);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Dimension originalSize = super.getPreferredSize();
-        int gap = (int) (originalSize.height * 0.6);
-        int x = originalSize.width + gap;
-        int y = gap;
-        int diameter = originalSize.height - (gap * 2);
-
-        g.setColor(Color.cyan);
-        g.fillOval(x, y, diameter, diameter);
-
-    }
-}
---
+++ src/com/sokoldv/bthost/BluetoothActionService.java
@@ -0,0 +1,225 @@
+package com.sokoldv.bthost;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+
+/**
+ * Created by UnetDeveloper on 02.11.2017.
+ */
+
+class BluetoothActionService implements DiscoveryListener {
+    private UUID uuid = new UUID("7f07d9800c8b4a13a087223c0f5e6109", false);
+    private LocalDevice localDevice;
+    private DiscoveryAgent agent;
+    private String serviceUrl = null;
+
+    private Vector<RemoteDevice> devices = new Vector<>();
+    private final Object lock = new Object();
+
+    private final StreamConnection connection;
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+
+
+//    private final Thread pingThread;
+
+    interface ActionCallback {
+        void onActionReply(String newAction);
+    }
+
+    BluetoothActionService() throws IOException {
+        localDevice = LocalDevice.getLocalDevice();
+        agent = localDevice.getDiscoveryAgent();
+
+        agent.startInquiry(DiscoveryAgent.GIAC, this);
+        synchronized (lock){
+            try { lock.wait(); }
+            catch (InterruptedException e) { e.printStackTrace(); }
+        }
+
+        serviceUrl = getServiceUrl();
+        if (serviceUrl == null) {
+            System.out.println("No service available");
+            System.exit(0);
+        }
+
+        connection = (StreamConnection) Connector.open(serviceUrl, Connector.READ_WRITE);
+        outputStream = connection.openDataOutputStream();
+        inputStream = connection.openDataInputStream();
+
+//        pingThread = new Thread(() -> {
+//            while (true){
+//                synchronized (BluetoothActionService.this){
+//                    try {
+//                        String message = "PING";
+//                        outputStream.writeByte(1);
+//                        outputStream.writeInt(message.length());
+//                        outputStream.writeUTF(message);
+//
+//                        inputStream.readByte();
+//                        inputStream.readInt();
+//
+//                        String utf = inputStream.readUTF();
+//                        if (!utf.equalsIgnoreCase("PING")){
+//
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//
+//                }
+//
+//                try {
+//                    wait(2000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        pingThread.start();
+    }
+
+    synchronized String getServiceUrl(){
+        if (serviceUrl != null)
+            return serviceUrl;
+
+        try {
+            serviceUrl = agent.selectService(uuid, ServiceRecord.AUTHENTICATE_ENCRYPT, true);
+        } catch (BluetoothStateException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(serviceUrl);
+        return serviceUrl;
+    }
+
+    synchronized void setAction(final String action, ActionCallback callback){
+
+        new Thread(() -> {
+//            String url = getServiceUrl();
+//            StreamConnection connection = null;
+            try {
+//                connection = (StreamConnection) Connector.open(serviceUrl, Connector.WRITE);
+//                DataOutputStream outputStream = connection.openDataOutputStream();
+
+                String message = "SET:ACTION:"+action;
+                outputStream.writeByte(1); // PACKET_STRING
+                outputStream.writeInt(message.length());
+                outputStream.writeUTF(message);
+
+                inputStream.readByte();
+                inputStream.readInt();
+                String newAction = inputStream.readUTF();
+                callback.onActionReply(newAction);
+
+//                outputStream.close();
+//                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            finally {
+//                if (connection != null){
+//                    try {
+//                        connection.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+        }).start();
+    }
+
+    synchronized void getAction(final ActionCallback callback){
+        new Thread(() -> {
+//            String url = getServiceUrl();
+//            StreamConnection connection = null;
+            try {
+//                connection = (StreamConnection) Connector.open(serviceUrl, Connector.READ_WRITE);
+//                DataOutputStream outputStream = connection.openDataOutputStream();
+//                DataInputStream inputStream = connection.openDataInputStream();
+
+                String message = "GET:ACTION";
+                outputStream.writeByte(1); // PACKET_STRING
+                outputStream.writeInt(message.length());
+                outputStream.writeUTF(message);
+
+                byte packet_type = inputStream.readByte();
+                if (packet_type != 1){
+                    System.out.println("Server should reply with PACKET_STRING instead of "+packet_type);
+                    return;
+                }
+
+                int len = inputStream.readInt();
+                String msg = inputStream.readUTF();
+
+                callback.onActionReply(msg.split(":")[2]);
+                System.out.println("Server said "+msg);
+//                outputStream.close();
+//                inputStream.close();
+//                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            finally {
+//                if (connection != null){
+//                    try {
+//                        connection.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+        }).start();
+    }
+
+    private void printDevices() {
+        int i=1;
+        for (RemoteDevice device : devices) {
+            System.out.print(String.format("#%d\t%s ", i++, device.getBluetoothAddress()));
+            try {
+                System.out.println(String.format("(%s)", device.getFriendlyName(false)));
+            } catch (IOException e) {
+                System.out.println();
+                e.printStackTrace();
+            }
+        }
+        System.out.println("end");
+    }
+
+    @Override
+    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+        devices.add(btDevice);
+    }
+
+    @Override
+    public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+
+    }
+
+    @Override
+    public void serviceSearchCompleted(int transID, int respCode) {
+
+    }
+
+    @Override
+    public void inquiryCompleted(int discType) {
+        System.out.println("inquiryCompleted");
+        synchronized (lock){
+            lock.notify();
+        }
+    }
+}
